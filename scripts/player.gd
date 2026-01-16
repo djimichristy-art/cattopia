bb63093c116f8c4d6587c1ba84951d8acc86815b
djimichristy-art <djimichristy@gmail.com>
2026-01-16 09:29:31 +0100

    painted the scene, player movements; jump, move right, left, added coins to the game.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -400
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+func _physics_process(delta) :
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.é
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -18,13 +17,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	if direction:
+		
+ 	
+ 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
